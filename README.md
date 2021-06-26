ba64bbfd1b6bd32a939b299aae00bf67056e0f07
kingreatwill <350840291@qq.com>
2021-06-26 13:32:05 +0800

    vscode 插件开发

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # vscode.extensions
 vscode extensions
+https://code.visualstudio.com/api
+
+## Get Started
+```
+npm install -g yo generator-code
+
+yo code
+```
+
+## Publishing
+```
+npm install -g vsce
+
+vsce package
+
+vsce publish
+```
+
+## npm
+
+安装但不写入package.json；
+npm install xxx
+
+安装并写入package.json的"dependencies"中 （--save）
+npm install axios -S 
+
+安装并写入package.json的"devDependencies"中（--save-dev）
+npm install xxx -D
+
+全局安装
+npm install xxx -g
+
+安装指定版本
+npm install xxx@1.2.0
+
+检查更新，可以看到所有可以更新的模块。
+npm outdated
+
+更新但不写入package.json；（其它的同上）
+npm update xxx
+
+删除指定模块；
+npm uninstall crawler 
+
+删除全局模块
+npm uninstall -g xxx
+
+查看全局安装的包
+npm list -g --depth 0
---
+++ stock/src/extension.ts
@@ -0,0 +1,121 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+//import { getStockInfo, clearIntervalTimer } from 'getStock.js';
+
+import axios from 'axios';
+
+let codeMap = new Map<string, string>();
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {	
+	const codes = vscode.workspace.getConfiguration().get<string>("codes");
+	let color = vscode.workspace.getConfiguration().get<string>("color");
+	if (!codes) {
+		return;
+	}
+	if (!color) {
+		color = "";
+	}
+	codes.split(",").forEach((code: string, index: number, array: Array<string>) => {
+		codeMap.set(code, code);
+	});
+
+	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 200);
+	statusBarItem.command = 'extension.stock.off';
+	statusBarItem.tooltip = '';
+	statusBarItem.color = color;//"#41464b";
+	context.subscriptions.push(vscode.commands.registerCommand('extension.stock', () => {
+		
+		statusBarItem.show();
+		statusBarItem.text = "loding...";
+		codeMap.forEach((value, key) => { 
+			getStockInfo(key, (codeBack: string, textBack: string) => {
+				codeMap.set(codeBack, textBack);
+				statusBarItem.text = tostr(codeMap);
+			});
+		});
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('extension.stock.off', () => {
+		clearIntervalTimer();
+		statusBarItem.text = '';
+		// exec(`open 'http://quote.eastmoney.com/'`);
+	}));
+
+	// let disposable = vscode.commands.registerCommand('stock.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from stock!');
+	// });
+
+	// context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
+
+function tostr(obj: Map<string, string>) {
+	let kv = "";	
+	obj.forEach((value, key) => { 
+		kv = kv + "  " + value;
+	});
+	return kv;
+}
+
+
+
+let timerMap = new Map<string, NodeJS.Timeout>();
+
+function getStockInfo(stockCode:string, callback:any) {
+	const instance = axios.create();
+	let updateTime = vscode.workspace.getConfiguration().get<number>('updateTime');
+	if(!updateTime || updateTime < 100){
+		updateTime = 1000;
+	}
+
+	let t = setInterval(() => {
+		let isAllFinish = true;
+		instance.get(`http://hq.sinajs.cn/list=${stockCode}`)
+          .then((response) => {
+				console.log(response.data);
+				const data =response.data;
+				const dataArr = data.split('="');
+				if (dataArr.length > 1) {
+					const dataArr1 = dataArr[1].split(',');
+					if (dataArr1.length > 3) {
+						const title = dataArr1[0];
+						const endPrice = dataArr1[2];
+						const price = dataArr1[3];
+						const rate = Math.round((price - endPrice) / endPrice * 10000) / 100;
+						isAllFinish = false;
+						callback(stockCode, `${parseFloat(price)}  ${rate}`);
+					}
+				}
+				if (isAllFinish) {
+					callback(stockCode, 'code不存在');
+					let t = timerMap.get(stockCode);
+					if(t){
+						clearInterval(t);
+					}
+				}
+          })
+          .catch((error) => {
+            callback(stockCode, 'loading ...');
+          });
+
+	}, updateTime);
+
+	timerMap.set(stockCode,t);
+}
+
+function clearIntervalTimer() {	
+	timerMap.forEach((value, key) => { 
+		let t = timerMap.get(key);
+		if(t){
+			clearInterval(t);
+		}
+	});
+}
+
